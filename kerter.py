c4757e45c915a23f3a37d8fb0b3b340b65d99f53
máté <mate.barath123@gmail.com>
2020-10-07 10:45:53 +0200

    modos

--- kerter.py
+++ kerter.py
@@ -1,19 +1,29 @@
+print("Mely síkidom kerületét és területét szeretné kiszámíttatni?")
+szam=float(input("szam: "))
+
 #py
 def korkerulet(r):
     return 2*r*3.14
 def korter(r):
     return r**2*3.14
-szam=float(input("szam: "))
-print(korkerulet(szam),"cm")
-print(korter(szam),"cm2")
+def nyolcszogTerulet (a,r):
+    return float (4*a*r)
+def nyolcszogKerulet (a):    
+    return float (8*a) 
 
-def nyolcszogTerulet ():
-a=float(input("Nyolcszög oldala [cm]: ")
-r=float(input("Nyolcszög sugara [cm]: ")
-return float (4*a*r)
+if szam==2:
+    ertek=float(input("sugar: "))
+    print(korkerulet(ertek),"cm")
+    print(korter(ertek),"cm2")
+elif szam==4:
+    a=float(input("Nyolcszög oldala [cm]: "))
+    r=float(input("Nyolcszög sugara [cm]: "))
+    print(nyolcszogTerulet(a,r))
+    print(nyolcszogKerulet (a))
+elif szam==1:
+    print("passz")
+else:
+    print("passz")
+	
 
-def nyolcszogKerulet ():
-a=float(input("Nyolcszög oldala [cm]: "))
-return float (8*a) 
 
-print()
