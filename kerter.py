2a711704290176cc2d64ab15e886de4f0ff9ce7a
máté <mate.barath123@gmail.com>
2020-10-07 10:13:28 +0200

    kesz

--- kerter.py
+++ kerter.py
@@ -1,12 +1,8 @@
 #py
-<<<<<<< HEAD
 def korkerulet(r):
     return 2*r*3.14
 def korter(r):
     return r**2*3.14
-#szam=float(input("szam: "))
-print(korkerulet(szam))
-print(korter(szam))
-=======
-
->>>>>>> 996c59485eff66dabe6d409fea85fc2f3ec34797
+szam=float(input("szam: "))
+print(korkerulet(szam),"cm")
+print(korter(szam),"cm2")
