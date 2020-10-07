5f10a5cb8b754e571f411e3defb82d2f1af45133
CserhatiDavid <csacsd@gmail.com>
2020-10-07 10:19:16 +0200

    nem tudom hogy nekem kene az inputot csinalni...majd megoldjuk

--- kerter.py
+++ kerter.py
@@ -1,2 +1,9 @@
 #py
-
+a=float(input("adja meg az egyik oldalt"))
+b=float(input("adja meg az másik oldalt"))
+def teglalapKerulet(kerulet):
+	kerulet=(a+b)*2
+	return kerulet
+def teglalapTerulet(terulet):
+	terulet=a*b
+	return terulet
